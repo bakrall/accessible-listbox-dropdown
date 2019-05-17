2c2c45a6b9a2f7461dc3c75385aaebc74b275be1
Barbara Stadnik <barbara1stadnik@gmail.com>
2019-05-17 07:29:37 +0200

    make the code functional, fix bug on opening list
    bug - when I opened list no 'itemSelected' was assigned, so I had to
    press down arrow twice to move forward

--- js/main.js
+++ js/main.js
@@ -1,58 +1,66 @@
 const selectionButton = document.querySelector('#selection__button'),
 	selectionList = document.querySelector('#selection__list'),
-	selectionItems = Array.from(document.querySelectorAll('.selection__item'));
+	selectionItems = Array.from(document.querySelectorAll('.selection__item')),
+    firstListItem = selectionItems[0],
+    lastListItem = selectionItems[selectionItems.length - 1];
 let itemSelected;
 
 function openSelectionList() {
 	selectionList.classList.toggle('active');
-	selectionList.firstElementChild.classList.add('selected');
+    selectFirstItem(firstListItem);
 }	
+
+function deSelectItem(listItem) {
+    listItem.classList.remove('selected');
+    listItem.removeAttribute('aria-selected', 'true');
+}
+
+function selectItem(listItem) {
+    listItem.classList.add('selected');
+    listItem.setAttribute('aria-selected', 'true');
+}
+
+function selectFirstItem(firstListItem) {
+    itemSelected = firstListItem;
+    selectItem(itemSelected);
+}
+
+function selectLastItem(lastListItem) {
+    itemSelected = lastListItem;
+    selectItem(itemSelected);
+}
 
 //https://stackoverflow.com/questions/8902787/navigate-through-list-using-arrow-keys-javascript-jq
 function moveFocus(e) {
     if(e.which === 40) {
         if(itemSelected) {
-            itemSelected.classList.remove('selected');
-            itemSelected.removeAttribute('aria-selected', 'true');
+            deSelectItem(itemSelected);
             next = itemSelected.nextElementSibling;
             if(next) {
                 itemSelected = next;
-                next.classList.add('selected');
-                next.setAttribute('aria-selected', 'true');
+                selectItem(next);
             } else {
-                itemSelected = selectionItems[0];
-                itemSelected.classList.add('selected');
-                itemSelected.setAttribute('aria-selected', 'true');
+                selectFirstItem(firstListItem);
             }
         } else {
-            itemSelected = selectionItems[0];
-            itemSelected.classList.add('selected');
-            itemSelected.setAttribute('aria-selected', 'true');
+            selectFirstItem(firstListItem);
         }
-
-        selectionList.setAttribute('aria-activedescendant', itemSelected.getAttribute('id'));
     } else if(e.which === 38) {
         if(itemSelected) {
-            itemSelected.classList.remove('selected');
+            deSelectItem(itemSelected);
             next = itemSelected.previousElementSibling;
-            itemSelected.removeAttribute('aria-selected', 'true');
             if(next) {
                 itemSelected = next;
-                next.classList.add('selected');
-                next.setAttribute('aria-selected', 'true');
+                selectItem(next);
             } else {
-                itemSelected = selectionItems[selectionItems.length - 1];
-                itemSelected.classList.add('selected');
-                itemSelected.setAttribute('aria-selected', 'true');
+                selectLastItem(lastListItem);
             }
         } else {
-            itemSelected = selectionItems[selectionItems.length - 1];
-            itemSelected.classList.add('selected');
-            itemSelected.setAttribute('aria-selected', 'true');
+            selectLastItem(lastListItem);
         }
-
-        selectionList.setAttribute('aria-activedescendant', itemSelected.getAttribute('id'));
     }
+
+    selectionList.setAttribute('aria-activedescendant', itemSelected.getAttribute('id'));
 };
 
 selectionButton.addEventListener('click', openSelectionList);
